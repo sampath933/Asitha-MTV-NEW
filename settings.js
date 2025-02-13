1b0134fd8cec6482925e36218a5b1f90050fa6a8
sampath933 <89699004+sampath933@users.noreply.github.com>
2025-02-13 12:16:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=iuwigCTT#bkaPDX3pLlcPtFtyqEos722iqIygoEiu16wMeUf_2R8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=f3YhwBiT#EU1xgLrb4xgPvS7KYNKM-y5-1FqVAxipVEB8ETL-BIw' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
